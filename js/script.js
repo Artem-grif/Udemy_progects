e8c70dd88199635a5a5f4eedba5ea2f6d5e4d429
Artem <zloiInko@gmail.com>
2021-04-08 21:34:13 +0200

    Lesson 26. Динамическая типизация в JS

--- js/script.js
+++ js/script.js
@@ -1,25 +1,56 @@
 'use strict';
 
-//Lesson 25. Отлавливаем ошибки в своем коде при помощи консоли разработчика. Breakpoints
+//Lesson 26. Динамическая типизация в JS
 
-function hello() {
-    console.log('Hello world!');
-    debugger;
+// To String
+
+// 1)
+// console.log(typeof(String(null)));
+// console.log(typeof(String(4)));
+
+// 2)
+console.log(typeof(5 + ''));
+
+const num = 5;
+
+console.log('https://vk.com/catalog/' + num);
+
+const fontSize = 26 + 'px';
+
+// To Number
+
+// 1)
+console.log(typeof(Number('4')));
+
+// 2)
+console.log(typeof(+'4'));
+
+// 3)
+console.log(typeof(parseInt('15px', 10)));
+
+
+let answ = +prompt('Hello', '');
+
+
+// To boolean
+
+// 0, '', null, undefined, NaN;
+
+// 1)
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...');
 }
 
-hello();
+switcher = 1;
 
-function hi() {
-    console.log('Say hi!');
+if (switcher) {
+    console.log('Working...');
 }
 
-hi();
+// 2)
+console.log(typeof(Boolean('4')));
 
-const arr = [1, 14, 4, 30, 54],
-    sorted = arr.sort(compareNum);
-
-function compareNum(a, b) {
-    return a - b;
-}
-
-console.log(sorted);
+// 3)
+console.log(typeof(!!'44444'));
