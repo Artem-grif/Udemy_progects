1182671ced4b7225013dd699f0fc8466f04f2029
Artem-grif <artem.osmak@ukr.net>
2021-04-21 21:37:01 +0200

    esson 29. Действия с элементами на странице

--- elements/js/script.js
+++ elements/js/script.js
@@ -1,25 +1,54 @@
 'use strict';
 
-// Lesson 28. Получение элементов со страницы
+// Lesson 29. Действия с элементами на странице
 
-const box = document.getElementById('box');
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      wrapper = document.querySelector('.wrapper'),
+      hearts = wrapper.querySelectorAll('.heart'),
+      oneHeart = wrapper.querySelector('.heart');
 
-console.log(box);
+// box.style.backgroundColor = 'blue';
+// box.style.width = '400px';
 
-const btns = document.getElementsByTagName('button');
+box.style.cssText = `background-color: blue; width: 400px`;
 
-console.log(btns[1]);
+btns[1].style.borderRadius = '35%';
+circles[1].style.backgroundColor = 'green';
+circles[2].style.backgroundColor = 'yellow';
 
-const circles = document.getElementsByClassName('circle');
-
-console.log(circles); 
-
-const hearts = document.querySelectorAll('.heart');
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
 
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor = 'blue';
 });
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут был я');
 
+div.classList.add('black');
+
+wrapper.append(div);
+// wrapper.appendChild(div);
+
+// wrapper.prepend(div);
+
+// hearts[1].before(div);
+// hearts[1].after(div);
+
+// wrapper.insertBefore(div, hearts[1]);
+
+// circles[0].remove();
+// wrapper.removeChild(hearts[1]);
+
+circles[1].replaceWith(hearts[0]);
+// wrapper.replaceChild(btns[1], hearts[0]);
+
+div.innerHTML = '<h1>Hello World</h1>';
+div.style.cssText = 'width: auto; color: blue';
+
+// div.textContent = '<h1>Hello World</h1>';
+div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');
